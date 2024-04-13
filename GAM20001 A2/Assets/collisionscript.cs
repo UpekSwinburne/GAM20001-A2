f1de2b25be942064741d267a0c7131c4beb4be04
UpekSwinburne <166790345+UpekSwinburne@users.noreply.github.com>
2024-04-13 23:30:50 +1000

    collision script
    added script for collision physics

---
+++ GAM20001 A2/Assets/collisionscript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionscript : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            print("HIT");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            print("STAY");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            print("EXIT");
+        }
+    }
+}
+    // Start is called before the first frame update
+   
