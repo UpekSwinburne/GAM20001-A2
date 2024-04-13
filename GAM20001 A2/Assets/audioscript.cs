a8d00f94187a8e92242bbc425b9cf3edc843531e
UpekSwinburne <166790345+UpekSwinburne@users.noreply.github.com>
2024-04-13 22:50:56 +1000

    Update audioscript.cs

--- GAM20001 A2/Assets/audioscript.cs
+++ GAM20001 A2/Assets/audioscript.cs
@@ -1,26 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class audioscript : MonoBehaviour
 {
-
     public AudioSource source;
     public AudioClip clip;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Set the AudioClip to loop
         source.loop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) 
+        // Check if the "A" key is pressed
+        if (Input.GetKeyDown(KeyCode.A))
         {
+            // Play the AudioClip
             source.Play();
-        
         }
-        
     }
 }
