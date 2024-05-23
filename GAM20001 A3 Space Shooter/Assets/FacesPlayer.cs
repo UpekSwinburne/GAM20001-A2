d95be5f961152c82184ffb7d7d2eebe228feba45
UpekSwinburne <166790345+UpekSwinburne@users.noreply.github.com>
2024-05-23 17:13:28 +1000

    new script faceplayer
    made the enemy always face the player and turn at a slow rate

--- GAM20001 A3 Space Shooter/Assets/FacesPlayer.cs
+++ GAM20001 A3 Space Shooter/Assets/FacesPlayer.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FacesPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 90f; 
+    Transform player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player == null)
+        {
+            GameObject go = GameObject.Find("Player Spaceship");
+
+            if (go != null)
+            {
+                player = go.transform;
+                Debug.Log("Player found: " + player.name);
+            }
+        }
+
+        if (player == null)
+        {
+            Debug.Log("Player is null");
+            return;
+        }
+
+        Vector3 dir = player.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+
+       
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
